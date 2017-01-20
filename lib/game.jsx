09d82d0f6cacc956d2e87feb0c2361a29f79e5d4
Koki Takahashi <hakatasiloving@gmail.com>
2017-01-21 01:24:24 +0900

    Test implement utterance

--- lib/game.jsx
+++ lib/game.jsx
@@ -2,11 +2,44 @@ const React = require('react');
 const CSSModules = require('react-css-modules');
 const styles = require('./game.pcss');
 
+const {speechSynthesis, SpeechSynthesisUtterance} = window;
+
 class Game extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			voices: [],
+		};
+	}
+
+	componentDidMount() {
+		this.onvoicechanged = () => {
+			this.setState({
+				voices: speechSynthesis.getVoices(),
+			});
+
+			const utterance = new SpeechSynthesisUtterance('Hello.');
+			utterance.voice = this.state.voices.find((voice) => voice.name === 'Google US English');
+			speechSynthesis.speak(utterance);
+		};
+
+		speechSynthesis.addEventListener('voiceschanged', this.onvoicechanged);
+	}
+
+	componentWillUnmount() {
+		speechSynthesis.removeEventListener('voiceschanged', this.onvoicechanged);
+	}
+
 	render() {
 		return (
 			<div styleName="wrap">
 				<h1 styleName="head">unicode-karuta</h1>
+				<ul>
+					{this.state.voices.map((voice) => (
+						<li key={voice.voiceURI}>{voice.name}</li>
+					))}
+				</ul>
 			</div>
 		);
 	}
