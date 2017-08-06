3dc490ae25a68776cf37252f4fd93d23458821d7
Travis CI <contact@travis-ci.com>
2017-08-06 08:20:30 +0000

    Update build - f38e7cf2f5de568b81e198d1b5f7574c59bda0bd

--- lib/clock.jsx
+++ lib/clock.jsx
@@ -9,13 +9,38 @@ class Clock extends React.Component {
 
 		this.state = {
 			time: '00:00',
+			percentage: 0,
 		};
+
+		setInterval(() => {
+			this.setState({percentage: (this.state.percentage + 0.012) % 1});
+		}, 1000 / 30);
 	}
 
 	render() {
+		const progressAngle = (this.state.percentage * 0.65 + 0.175) * Math.PI * 2;
+
 		return (
 			<div styleName="clock">
 				<div styleName="circle">
+					<svg styleName="progress" viewBox="0 0 100 100">
+						<path
+							styleName="progress-path"
+							d={`
+								M 50 50
+								L 50 100
+								A
+									50
+									50
+									0
+									${this.state.percentage < 0.5 ? 0 : 1}
+									1
+									${-Math.sin(progressAngle) * 50 + 50}
+									${Math.cos(progressAngle) * 50 + 50}
+								Z
+							`}
+						/>
+					</svg>
 					<div styleName="inner-circle">
 						{this.state.time}
 					</div>
--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
 	plugins: [
 		require('precss')(),
 		require('postcss-calc')(),
+		require('postcss-font-magician')(),
 		require('autoprefixer')(),
 		require('postcss-flexbugs-fixes')(),
 	],
